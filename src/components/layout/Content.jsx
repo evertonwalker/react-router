86f263982348d95925ecd7d6bc8d70db48fe23bc
Everton <everton.walker@hotmail.com>
2021-04-28 14:41:00 -0300

    Route exact e component não encontrado

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -4,19 +4,23 @@ import { Switch, Route } from "react-router-dom";
 import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = (props) => {
   return (
     <main className="Content">
       <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route path="/param/:id">
           <Param />
         </Route>
         <Route path="/about">
           <About />
         </Route>
-        <Route path="/">
-          <Home />
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </main>
