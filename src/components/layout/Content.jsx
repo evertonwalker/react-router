28b7f72320e42d944015af3e81b7962cb16c220c
Everton <everton.walker@hotmail.com>
2021-04-28 11:54:43 -0300

    configurando projeto e roteamento

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Content.css";
+import { Switch, Route } from "react-router-dom";
+import About from "../../views/examples/About";
+import Home from "../../views/examples/Home";
+import Param from "../../views/examples/Param";
+
+const Content = (props) => {
+  return (
+    <main className="Content">
+      <Switch>
+        <Route path="/param/:id">
+          <Param />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </main>
+  );
+};
+
+export default Content;
