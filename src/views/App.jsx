4ad08234be1ff8dd3c4728cc42e6552823f0cd7b
Everton <everton.walker@hotmail.com>
2021-05-17 10:48:03 -0300

    maneira mais inteligente de usar o ContextAPi

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -5,19 +5,22 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Menu from "../components/layout/Menu";
 import Content from "../components/layout/Content";
 import DataContext, { data } from "../data/DataContext";
+import Store from "../data/Store";
 
 const App = (props) => {
   const [context, setStateContext] = useState(data);
 
   return (
-    <DataContext.Provider value={{ context, setStateContext }}>
-      <div className="App">
-        <Router>
-          <Menu />
-          <Content />
-        </Router>
-      </div>
-    </DataContext.Provider>
+    <Store>
+      <DataContext.Provider value={{ context, setStateContext }}>
+        <div className="App">
+          <Router>
+            <Menu />
+            <Content />
+          </Router>
+        </div>
+      </DataContext.Provider>
+    </Store>
   );
 };
 
--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,16 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 import DataContext from "../../data/DataContext";
+import { AppContext } from "../../data/Store";
 
 const UseContext = (props) => {
   const { context, setStateContext } = useContext(DataContext);
 
-  function setNumber(value) {
+  const { number, setNumber, text, setText } = useContext(AppContext);
+
+  function addNumber(value) {
     setStateContext({
       ...context,
       number: context.number + value,
     });
   }
+
+  useEffect(() => {
+    if (number > 1250) {
+      setText("Eita!");
+    }
+  }, [number]);
 
   return (
     <div className="UseContext">
@@ -18,18 +28,35 @@ const UseContext = (props) => {
         title="Hook UseContext"
         subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
       />
+
+      <SectionTitle title="Exercício #01" />
       <div className="center">
         <span className="text">{context.text}</span>
         <span className="text">{context.number}</span>
+        <div>
+          <button className="btn" onClick={() => addNumber(-1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => addNumber(1)}>
+            +1
+          </button>
+        </div>
       </div>
 
+      <SectionTitle title="Exercício #02" />
       <div className="center">
-        <button className="btn" onClick={() => setNumber(-1)}>
-          -1
-        </button>
-        <button className="btn" onClick={() => setNumber(1)}>
-          +1
-        </button>
+        <span className="text">{number}</span>
+        <div>
+          <button className="btn" onClick={() => setNumber(number - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setNumber(number + 1)}>
+            +1
+          </button>
+        </div>
+      </div>
+      <div className="center">
+        <span className="text"> {text}</span>
       </div>
     </div>
   );
